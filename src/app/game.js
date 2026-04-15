e58e6388fcd68b4bb8ce651da13bdf0d2901c639
raul-g-ferreira <raul.gferreira7@gmail.com>
2026-04-15 11:09:44 -0300

    Adicionado rotinas de vitória e empate

--- src/app/game.js
+++ src/app/game.js
@@ -12,9 +12,46 @@ function select(e) {
         console.log('seleciona')
 
         field.setAttribute(player, '')
-    }
 
-    player1Turn = !player1Turn
+        player1Turn = !player1Turn
+
+        checkVictory(player)
+        checkDraw()
+    }
+}
+
+function showVictory(player) {
+    alert(`Vitória da ${playerNames[player == 'player1'? 0 : 1]}`)
+    reset() // reload
+}
+
+function checkDraw() {
+    const occupiedPositions = Array.from(document.querySelectorAll('[player1], [player2]'))
+    if (occupiedPositions.length >= 9) {
+        alert('Deu velha!')
+        reset()
+    }
+}
+
+function checkVictory(player) {
+    const playerPositions = Array.from(document.querySelectorAll(`[${player}]`)).map(pos => pos.id)
+
+    const winningPositions = [
+        ['id1', 'id2', 'id3'], ['id4', 'id5', 'id6'], ['id7', 'id8', 'id9'],
+        ['id1', 'id4', 'id7'], ['id2', 'id5', 'id8'], ['id3', 'id6', 'id9'],
+        ['id1', 'id5', 'id9'], ['id3', 'id5', 'id7']
+    ]
+
+    const isSubset = (wLineup) => wLineup.every(pos => playerPositions.includes(pos))
+
+    const winnerLineup = winningPositions.filter(wLineup => {
+        return isSubset(wLineup)
+    })
+
+    if (winnerLineup.length > 0) { // debug
+        console.log(`${player} wins`)
+        showVictory(player)
+    }
 }
 
 function initialize() {
@@ -25,9 +62,9 @@ function initialize() {
         document.querySelector('.game-window').appendChild(div)
     }
     const divs = document.querySelectorAll('div .position')
-    .forEach(div => {
-        div.onclick = select
-    })
+        .forEach(div => {
+            div.onclick = select
+        })
 }
 
 function reset() {
@@ -35,4 +72,5 @@ function reset() {
 }
 
 var player1Turn = true
+const playerNames = ['Honda', 'Yamaha']
 initialize()
